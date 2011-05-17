0f7601ed1184ac8b466ad6ba6c1be4962ca4fd36
Ulli Hafner <ullrich.hafner@gmail.com>
2011-05-17 10:42:43 +0200

    Changed signature of aggregation methods.

--- src/main/java/hudson/plugins/dry/DryMavenResultAction.java
+++ src/main/java/hudson/plugins/dry/DryMavenResultAction.java
@@ -70,8 +70,8 @@ public class DryMavenResultAction extends MavenResultAction<DryResult> {
     }
 
     @Override
-    protected DryResult createResult(final DryResult... results) {
-        return new DryResult(getOwner(), results[0].getDefaultEncoding(), aggregate(results));
+    protected DryResult createResult(final DryResult existingResult, final DryResult additionalResult) {
+        return new DryResult(getOwner(), additionalResult.getDefaultEncoding(), aggregate(existingResult, additionalResult));
     }
 }
 
